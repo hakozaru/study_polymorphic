b1ce10b65f4bd1f59878f9d9d821e9a4acd20a09
vagrant <vagrant@localhost.localdomain>
2016-07-30 05:32:00 +0000

    generate 3 model and run migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160730052923) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "page",       limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "release",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
